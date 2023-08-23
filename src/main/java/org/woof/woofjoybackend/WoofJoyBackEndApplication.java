7a9354f4e7cec3965b8b7b0a89ac2c48cdd0b5c2
pedro-l0pes <pedro.lopes@sptech.school>
2023-08-23 19:30:07 -0300

    Commit inicial

---
+++ src/main/java/org/woof/woofjoybackend/WoofJoyBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.woof.woofjoybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoofJoyBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoofJoyBackEndApplication.class, args);
+	}
+
+}
