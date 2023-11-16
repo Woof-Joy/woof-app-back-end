e69f7383b53f431441a76c753d8d17192c85a84f
pedro-l0pes <pedro.lopes@sptech.school>
2023-11-16 17:45:26 -0300

    Atualizando entidade residencia

--- src/main/java/org/woof/woofjoybackend/entity/Residencia.java
+++ src/main/java/org/woof/woofjoybackend/entity/Residencia.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jdk.jfr.BooleanFlag;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,9 +20,14 @@ public class Residencia {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idResidencia;
     private String tipoResidencia;
+    @BooleanFlag
     private Boolean areaExterna;
+    @BooleanFlag
     private Boolean temAnimais;
+    @BooleanFlag
     private Boolean temCriancas;
+    @BooleanFlag
     private Boolean rotaFuga;
+    @BooleanFlag
     private Boolean dogSofaCama;
 }
