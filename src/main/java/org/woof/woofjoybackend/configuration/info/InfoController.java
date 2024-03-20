10b0c801239e100562aa9e9ed7e750e0ff2921f7
FilipeRicardoSilva <filipe.silva@sptech.school>
2024-03-20 15:08:52 -0300

    feat: add info/java controller

--- src/main/java/org/woof/woofjoybackend/configuration/info/InfoController.java
+++ src/main/java/org/woof/woofjoybackend/configuration/info/InfoController.java
@@ -1,4 +1,4 @@
-package org.woof.woofjoybackend.configuration.security.info;
+package org.woof.woofjoybackend.configuration.info;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
