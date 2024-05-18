0c89778e136305c0e901aad36a25a879918317b2
FilipeRicardoSilva <filipe.silva@sptech.school>
2024-05-18 13:22:01 -0300

    test COMENT S3

--- src/main/java/org/woof/woofjoybackend/s3connect/controller/StorageController.java
+++ src/main/java/org/woof/woofjoybackend/s3connect/controller/StorageController.java
@@ -1,60 +1,60 @@
-package org.woof.woofjoybackend.s3connect.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.woof.woofjoybackend.domain.entity.DonoImagem;
-import org.woof.woofjoybackend.s3connect.service.StorageService;
-import org.woof.woofjoybackend.service.users.ServiceUser;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/img")
-@RequiredArgsConstructor
-public class StorageController {
-
-    private final StorageService service;
-    private final ServiceUser serviceUser;
-
-
-    @GetMapping("/connection")
-    public ResponseEntity<Boolean> connectionHealthCheck() {
-        Boolean estado = service.connectionHealthCheck();
-        return ResponseEntity.status(200).body(estado);
-    }
-
-    @PostMapping("/upload/{tipo}")
-    public ResponseEntity<String> uploadProfileImg(@PathVariable String tipo, @RequestParam(value = "file") MultipartFile file, @RequestHeader("Authorization") String bearerToken) {
-        DonoImagem dono = serviceUser.getDonoByToken(bearerToken);
-        if (dono == null) {
-            return ResponseEntity.notFound().build();
-        }
-        if (tipo.equalsIgnoreCase("perfil")) {
-            return new ResponseEntity<>(service.uploadProfileImg(file, dono.getId()), HttpStatus.OK);
-        } else if (tipo.equalsIgnoreCase("img")) {
-            return new ResponseEntity<>(service.uploadImg(file, dono.getId()), HttpStatus.OK);
-        }
-        return ResponseEntity.badRequest().build();
-    }
-
-    @GetMapping("/url/{tipo}")
-    public ResponseEntity<List<String>> getImgUrl(@PathVariable String tipo, @RequestHeader("Authorization") String bearerToken) {
-        DonoImagem dono = serviceUser.getDonoByToken(bearerToken);
-        if (dono == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok().body(service.getImgUrl(dono.getId(), tipo));
-    }
-
-    @DeleteMapping("/delete/{fileName}")
-    public ResponseEntity<String> deleteFile(@PathVariable String fileName, @RequestHeader("Authorization") String bearerToken) {
-        DonoImagem dono = serviceUser.getDonoByToken(bearerToken);
-        if (dono == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return new ResponseEntity<>(service.deleteFile(fileName, dono.getId()), HttpStatus.OK);
-    }
-}
+//package org.woof.woofjoybackend.s3connect.controller;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.multipart.MultipartFile;
+//import org.woof.woofjoybackend.domain.entity.DonoImagem;
+//import org.woof.woofjoybackend.s3connect.service.StorageService;
+//import org.woof.woofjoybackend.service.users.ServiceUser;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/img")
+//@RequiredArgsConstructor
+//public class StorageController {
+//
+//    private final StorageService service;
+//    private final ServiceUser serviceUser;
+//
+//
+//    @GetMapping("/connection")
+//    public ResponseEntity<Boolean> connectionHealthCheck() {
+//        Boolean estado = service.connectionHealthCheck();
+//        return ResponseEntity.status(200).body(estado);
+//    }
+//
+//    @PostMapping("/upload/{tipo}")
+//    public ResponseEntity<String> uploadProfileImg(@PathVariable String tipo, @RequestParam(value = "file") MultipartFile file, @RequestHeader("Authorization") String bearerToken) {
+//        DonoImagem dono = serviceUser.getDonoByToken(bearerToken);
+//        if (dono == null) {
+//            return ResponseEntity.notFound().build();
+//        }
+//        if (tipo.equalsIgnoreCase("perfil")) {
+//            return new ResponseEntity<>(service.uploadProfileImg(file, dono.getId()), HttpStatus.OK);
+//        } else if (tipo.equalsIgnoreCase("img")) {
+//            return new ResponseEntity<>(service.uploadImg(file, dono.getId()), HttpStatus.OK);
+//        }
+//        return ResponseEntity.badRequest().build();
+//    }
+//
+//    @GetMapping("/url/{tipo}")
+//    public ResponseEntity<List<String>> getImgUrl(@PathVariable String tipo, @RequestHeader("Authorization") String bearerToken) {
+//        DonoImagem dono = serviceUser.getDonoByToken(bearerToken);
+//        if (dono == null) {
+//            return ResponseEntity.notFound().build();
+//        }
+//        return ResponseEntity.ok().body(service.getImgUrl(dono.getId(), tipo));
+//    }
+//
+//    @DeleteMapping("/delete/{fileName}")
+//    public ResponseEntity<String> deleteFile(@PathVariable String fileName, @RequestHeader("Authorization") String bearerToken) {
+//        DonoImagem dono = serviceUser.getDonoByToken(bearerToken);
+//        if (dono == null) {
+//            return ResponseEntity.notFound().build();
+//        }
+//        return new ResponseEntity<>(service.deleteFile(fileName, dono.getId()), HttpStatus.OK);
+//    }
+//}
